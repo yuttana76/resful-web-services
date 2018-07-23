eab3db1fea45bfc7e551ebec17f59f33d1e7dd2c
Yuttana <yuttana76>
2018-07-23 12:04:39 +0700

    Add Delete function

--- src/main/java/com/example/rest/webservices/resfulwebservices/user/UserDaoService.java
+++ src/main/java/com/example/rest/webservices/resfulwebservices/user/UserDaoService.java
@@ -2,6 +2,7 @@ package com.example.rest.webservices.resfulwebservices.user;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -39,4 +40,18 @@ public class UserDaoService {
 		}
 		return null;
 	}
+	
+	public User deleteById(int id){
+		Iterator<User> iterator = users.iterator();
+		
+		while(iterator.hasNext()){
+			User user = iterator.next();
+			if(user.getId() == id){
+				iterator.remove();
+				return user;
+			}
+		}
+		return null;
+	}
+	
 }
